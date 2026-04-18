3ac5f311d6e44ffc3447d53b034094feaf6f13d9
Alan-Schzhz <alansanchezhdez853@gmail.com>
2026-04-17 20:15:35 -0600

    c_no_opt.cc

--- src/c_no_opt.cc
+++ src/c_no_opt.cc
@@ -1,5 +1,5 @@
 /* * Proyecto: Garra Robótica con Joysticks
- * Versión: 1.1.0 - Etapa 2 (Funcionalidad Básica)
+ * Versión: 1.2.0 - Etapa 2 (Funcionalidad Básica)
  * Fecha: Abril 2026
  * Autores: [Hernandez Castro Luis Adrian // Juan Gonzalez David // Maroto May Alejandro // Sanchez Hernandez Alan // Zavala Cervantes Luis Daniel]
  */
@@ -19,5 +19,11 @@ void loop() {
   int valBase = analogRead(A0);
   int valHombro = analogRead(A1);
   int valCodo = analogRead(A2);
-  int valGarra = analogRead(A3)
+  int valGarra = analogRead(A3) ;
+  //[Sánchez Hernandez Alan]
+  // Convertir valores analógicos (0-1023) a grados (0-180)
+  int targetBase = map(valBase, 0, 1023, 0, 180);
+  int targetHombro = map(valHombro, 0, 1023, 45, 135); // Restringido para proteger estructura
+  int targetCodo = map(valCodo, 0, 1023, 0, 180);
+  int targetGarra = map(valGarra, 0, 1023, 10, 90);   // Restringido para la pinza
 }
